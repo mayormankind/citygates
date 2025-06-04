f65f65b883d846b682c2afc26e3b896d683a62d5
Jerry <mayowamakinde23@gmail.com>
2025-06-04 13:27:41 +0100

    update

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,7 +8,8 @@ export default function About() {
             <section className='w-full'>
                 <Image alt='A man putting a coin in a jar' src='/growth.jpg' width={1000} height={1000}/>
             </section>
-            <section className='w-full flex p-8 md:p-12 bg-blue-900 text-white'>
+            {/* <section className='w-full flex p-8 md:p-12 bg-blue-900 text-white'> */}
+            <section className='w-full bg-gray-200 flex p-8 md:p-12 text-black'>
                 <div className="flex flex-col gap-4 my-auto">
                     <h1 className='text-3xl font-bold'>About Us?</h1>
                     <p>CityGates Food Bank provides strategic food supply chain and financial support in an ethical cooperative manner with a view to eradicate poverty and ensure food security in the land. We also grant instant loan to men and women in business and career to solve their immediate financial issues. In a bid to touch the lives of the most vulnerable, we also go to the rural community to provide food stuff to vulnerable women and children through our communal food outreach programme.</p>
