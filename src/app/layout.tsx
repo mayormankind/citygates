16b649f24adb88fa05ddbc0d7ebb7d89bb37f929
mayowa <mayowamakinde23@gmail.com>
2025-07-24 16:51:08 +0100

    Add User context for user otp verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,3 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import Header from "@/components/layout/header";
-// import Footer from "@/components/layout/footer";
-// import { Toaster } from "sonner";
-// import LoadingFallback from "@/components/layout/LoadingFallback";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//   title: "CityGates",
-//   description: "Your go-to loan and survival merchant",
-//   icons: "/globe.svg",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{ children: React.ReactNode }>) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         <LoadingFallback />
-//         <main>
-//           <Header />
-//           {children}
-//           <Footer />
-//         </main>
-//         <Toaster richColors position="top-center" />
-//       </body>
-//     </html>
-//   );
-// }
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -52,6 +8,7 @@ import { AdminProvider } from "@/context/AdminContext";
 import LoadingFallback from "@/components/layout/LoadingFallback";
 import ErrorBoundary from "@/components/layout/ErrorBoundary";
 import { BranchRoleProvider } from "@/context/BranchRoleContext";
+import { UserProvider } from "@/context/UserContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -79,16 +36,18 @@ export default function RootLayout({
       >
         <ErrorBoundary>
           <AdminProvider>
-            <BranchRoleProvider>
-              {" "}
-              <LoadingFallback />
-              <main>
-                <Header />
-                {children}
-                <Footer />
-              </main>
-              <Toaster richColors position="top-center" />
-            </BranchRoleProvider>
+            <UserProvider>
+              <BranchRoleProvider>
+                {" "}
+                <LoadingFallback />
+                <main>
+                  <Header />
+                  {children}
+                  <Footer />
+                </main>
+                <Toaster richColors position="top-center" />
+              </BranchRoleProvider>
+            </UserProvider>
           </AdminProvider>
         </ErrorBoundary>
       </body>
